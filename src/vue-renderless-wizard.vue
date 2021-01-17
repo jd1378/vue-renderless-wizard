c8a5121450143fc4906f7626c9457cd4b465a525
Javad Mnjd <javad.mnjd@hotmail.com>
2021-01-17 14:58:19 +0330

    add documentation
    add setStep
    add reset
    steps start from 1 now

--- src/vue-renderless-wizard.vue
+++ src/vue-renderless-wizard.vue
@@ -1,58 +1,97 @@
 <script>
+/**
+ * Vue Renderless Wizard component (hopefully) helps you manage the steps of your wizard easily.
+ * @example ./wizard-container.vue
+ */
 export default {
   props: {
+    /**
+     * Sets the step to show when rendering the wizard for the first time
+     */
     startingStep: {
       type: Number,
-      default: 0,
+      default: 1,
       validator(val) {
-        return val >= 0;
+        return val > 0;
       },
     },
   },
   data() {
     return {
-      currentStep: 0,
+      currentStep: 1,
     };
   },
   computed: {
+    /**
+     * the count of detected steps by enumerating $slots
+     * each defined template with 'step-{n}' as slot name counts as 1 step
+     */
     stepsCount() {
       return Object.keys(this.$slots).reduce((accumulator, slotName) => {
-        if (slotName.endsWith(accumulator)) {
+        if (slotName.startsWith('step-')) {
           return accumulator + 1;
         }
         return accumulator;
-      }, -1);
+      }, 0);
     },
   },
   created() {
     this.currentStep = this.startingStep;
   },
   methods: {
-    next(predicate = true) {
+    next() {
       if (this.currentStep < this.stepsCount) {
-        if (
-          (typeof predicate === 'boolean' && predicate) ||
-          (typeof predicate === 'function' && predicate())
-        ) {
-          this.currentStep++;
-        }
+        this.currentStep++;
+        return true;
       }
       return false;
     },
-    prev(predicate = true) {
-      if (this.currentStep > 0) {
-        if (
-          (typeof predicate === 'boolean' && predicate) ||
-          (typeof predicate === 'function' && predicate())
-        ) {
-          this.currentStep--;
-        }
+    prev() {
+      if (this.currentStep > 1) {
+        this.currentStep--;
+        return true;
       }
+      return false;
     },
-    getSlot(vnode) {
-      console.log(vnode);
+    setStep(step) {
+      this.currentStep = step;
+      /** Triggered when goTo is called with the new step
+       * @event setStep
+       * @type {Event}
+       */
+      this.$emit('setStep', step);
+    },
+    reset() {
+      this.currentStep = this.startingStep;
+      /** Triggered when reset is called and current step is changed to startingStep prop
+       * @event reset
+       * @type {Event}
+       */
+      this.$emit('reset');
     },
   },
+  /**
+   * You define your steps like step-1, step-2, ... and it will be rendered when you are on that step.
+   * @slot step-{n}
+   * @binding {function} next Proceed to next step
+   * @binding {function} prev Proceed to previous step
+   * @binding {number} currentStep
+   * @binding {number} stepsCount
+   * @binding {function} setStep Emits `setStep` event and goes to given step
+   * @binding {reset} reset Emits a `reset` event and goes to first step
+   */
+  /**
+   * The container of steps. you can use this slot to wrap the steps you define with the content of this slot.
+   * but for rendering the steps you have to use the VNodesRenderer helper component provided.
+   * @slot container
+   * @binding {Array<VNode>} stepVNodes VNodes of current step of wizard.
+   * @binding {function} next Proceed to next step
+   * @binding {function} prev Proceed to previous step
+   * @binding {number} currentStep
+   * @binding {number} stepsCount
+   * @binding {function} setStep Emits `setStep` event and goes to given step
+   * @binding {reset} reset Emits a `reset` event and goes to first step
+   */
   render(h) {
     let stepVNodes;
 
@@ -62,6 +101,8 @@ export default {
         prev: this.prev,
         currentStep: this.currentStep,
         stepsCount: this.stepsCount,
+        setStep: this.setStep,
+        reset: this.reset,
       });
     }
 
@@ -73,6 +114,8 @@ export default {
           stepsCount: this.stepsCount,
           next: this.next,
           prev: this.prev,
+          setStep: this.setStep,
+          reset: this.reset,
         });
         return containerVNode;
       }
