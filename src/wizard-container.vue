fdfd6b81ec02a79aca269cdaf24f561948c7d890
Javad Mnjd <javad.mnjd@hotmail.com>
2021-01-17 15:30:04 +0330

    use the new VNodeRenderer

--- src/wizard-container.vue
+++ src/wizard-container.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="wizard-container">
-    <VNodeRenderer :vnodes="wizard.vnodes" />
+    <div class="step-content">
+      <VNodeRenderer :vnodes="wizard.vnodes" />
+    </div>
+    <div class="step-controller"></div>
   </div>
 </template>
 
 <script>
-import { VNodeRenderer } from '@/vue-renderless-wizard.vue';
+import VNodeRenderer from '@/v-node-renderer.js';
+
 export default {
   components: {
     VNodeRenderer,
@@ -21,7 +25,21 @@ export default {
 
 <style lang="css">
 .wizard-container {
-  background: #232323;
-  color: #cdcdcd;
+  background: #f7f7f7;
+  border-radius: 3px;
+  box-shadow: 0px 0px 5px 0px rgba(35, 35, 35, 0.3);
+  color: #424242;
+  font-size: 1.2rem;
+  margin: 1rem;
+  min-height: 300px;
+
+  display: flex;
+  flex-direction: column;
+}
+
+.step-controller {
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem;
 }
 </style>
