673f981eda5c8197b271ac959d3ec6475daa98d4
Javad Mnjd <javad.mnjd@hotmail.com>
2023-05-22 06:21:40 +0330

    fix: enable vue devtools on dev

--- src/ServeDev.ts
+++ src/ServeDev.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue';
 import Dev from './ServeDev.vue';
 
-createApp(Dev).mount('#app');
+const app = createApp(Dev).mount('#app');
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+app.config.devtools = true;
